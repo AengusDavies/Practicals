cb742cd57d33bd15f31c746dde23ce09912ab3bb
AengusDavies <aengusdavies99@gmail.com>
2021-03-18 14:50:39 +1000

    Add password check program

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -8,6 +8,10 @@ length requirements
 def main():
     minimum_length = 5
     password = str(input("Enter a password: "))
+    get_password(minimum_length, password)
+
+
+def get_password(minimum_length, password):
     while len(password) < minimum_length:
         print("Password is not long enough")
         password = str(input("Enter a password: "))
