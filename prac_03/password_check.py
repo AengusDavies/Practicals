2b21f1f657d0ce31f6dcad5aeca84f3389959c40
AengusDavies <aengusdavies99@gmail.com>
2021-03-18 15:24:35 +1000

    Add password check program

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,22 +1,12 @@
 """
 CP1404 - Practical
-Program that asks user for password & prints as asterisks once it has met
-length requirements
+Program that validates a password, then prints as asterisks
 """
 
-
-def main():
-    minimum_length = 5
-    password = str(input("Enter a password: "))
-    get_password(minimum_length, password)
-
-
-def get_password(minimum_length, password):
-    while len(password) < minimum_length:
-        print("Password is not long enough")
-        password = str(input("Enter a password: "))
-    for i in range(len(password)):
-        print('*', end='')
-
-
-main()
+minimum_length = 5
+password = str(input("Enter password: "))
+while len(password) < minimum_length:
+    print("Invalid password")
+    password = str(input("Enter password: "))
+for i in range(len(password)):
+    print("*", end='')
