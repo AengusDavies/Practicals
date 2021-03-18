9c2ddde41ff4ab0a6010ee80f97f31ec67477514
AengusDavies <aengusdavies99@gmail.com>
2021-03-18 15:51:01 +1000

    Modify password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -4,10 +4,24 @@ Program that asks user for password & prints as asterisks once it has met
 length requirements
 """
 
-minimum_length = 5
-password = str(input("Enter password: "))
-while len(password) < minimum_length:
-    print("Invalid password")
+
+def main():
+    minimum_length = 5
     password = str(input("Enter password: "))
-for i in range(len(password)):
-    print("*", end='')
+    password = get_password(minimum_length, password)
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end='')
+
+
+def get_password(minimum_length, password):
+    while len(password) < minimum_length:
+        print("Invalid password")
+        password = str(input("Enter password: "))
+    return password
+
+
+main()
