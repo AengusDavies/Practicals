c3688d43ec0ccbf6267cccae3f4006874c4ef83a
AengusDavies <aengusdavies99@gmail.com>
2021-03-19 13:23:41 +1000

    Modify functions to have appropriate naming, and feature docstrings

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -8,16 +8,18 @@ length requirements
 def main():
     minimum_length = 5
     password = str(input("Enter password: "))
-    password = get_password(minimum_length, password)
+    password = validate_password(minimum_length, password)
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Prints asterisks based on password length"""
     for i in range(len(password)):
         print("*", end='')
 
 
-def get_password(minimum_length, password):
+def validate_password(minimum_length, password):
+    """Validates given password"""
     while len(password) < minimum_length:
         print("Invalid password")
         password = str(input("Enter password: "))
