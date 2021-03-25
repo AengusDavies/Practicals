32582fdf56ebbe2146d5ed61743d38e331b86eb3
AengusDavies <aengusdavies99@gmail.com>
2021-03-25 13:09:49 +1000

    Add data printing function to program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,13 +8,22 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_data(data)
+
+
+def print_data(data_lists):
+    """Print the acquired data"""
+    for data_list in data_lists:
+        subject_name = data_list[0]
+        lecturer_name = data_list[1]
+        number_of_students = data_list[2]
+        print("{} is taught by {} and has {} students".format(subject_name, lecturer_name, number_of_students))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    data = []
+    data_lists = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,10 +32,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
-        data.append(parts)
+        data_lists.append(parts)
         print("----------")
     input_file.close()
-    return data
+    return data_lists
 
 
 main()
