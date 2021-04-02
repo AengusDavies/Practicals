c522eb64215f051ca4a7d903b42b44e6f156bf62
AengusDavies <aengusdavies99@gmail.com>
2021-04-02 15:32:39 +1000

    Add word occurrences program to repo

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical
+Program that prints counts of each word in dictionary
+"""
+
+words_to_count = {}
+text = str(input("Text: "))
+words = text.split()
+for word in words:
+    try:
+        word_occurrences = words_to_count.get(word, 0)
+        words_to_count[word] = word_occurrences + 1
+    except KeyError:
+        words_to_count[word] = 1
+words = list(words_to_count.keys())
