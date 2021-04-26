e7456ad1b3cf49267025d0de5bcd60bd0b01e35d
AengusDavies <aengusdavies99@gmail.com>
2021-04-26 16:04:00 +1000

    Change handle functions to accommodate error checking function; add on_text functionality to Kivy app

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -13,20 +13,23 @@ KM_MULTIPLIER = 1.60934
 class MilesKmApp(App):
     output_text = StringProperty()
     """A Kivy app for converting miles to km"""
-
     def build(self):
+        """Load kv file in order to build Kivy app"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculation(self, value):
+    def handle_calculation(self):
         """Handle calculation for converting miles to kilometres"""
+        value = self.handle_invalid_input()
         result = value * KM_MULTIPLIER
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, addition):
         """Handle increment of +1 or -1 depending on button pressed was Up or Down"""
         new_value = self.handle_invalid_input() + addition
+        self.root.ids.input_number.text = str(new_value)
+        self.handle_calculation()
 
     def handle_invalid_input(self):
         """Handle an invalid input by turning it into a zero"""
@@ -34,7 +37,7 @@ class MilesKmApp(App):
             value = float(self.root.ids.input_number.text)
             return value
         except ValueError:
-            return 0.1
+            return 0.0
 
 
 MilesKmApp().run()
