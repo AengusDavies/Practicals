161ef47249588f3aa9bbf44d858f45fee891a1ed
AengusDavies <aengusdavies99@gmail.com>
2021-05-07 11:21:51 +1000

    Correct small ordering error in silver service taxi program

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -9,7 +9,7 @@ from prac_08.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
-    def __init__(self, fanciness, name, fuel):
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
