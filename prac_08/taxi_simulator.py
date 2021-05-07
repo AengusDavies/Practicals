5e473a665ffe9ea8bb7ba173f33f1f30438855bf
AengusDavies <aengusdavies99@gmail.com>
2021-05-07 12:04:39 +1000

    Add taxi simulator program to repo

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 Practical
+Taxi Simulator Program
+"""
+
+from prac_08.car import Car
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            pass
+        elif menu_choice == "d":
+            pass
+
+
+main()
