287e4750b044ca33e8a53f5f5cef4ea4721dc2c7
AengusDavies <aengusdavies99@gmail.com>
2021-05-07 12:46:47 +1000

    Add drive and trip handling for chosen taxi option to taxi simulator program

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -12,6 +12,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    bill = 0
     current_taxi = None
     print("Let's drive!")
     print(MENU)
@@ -27,12 +28,20 @@ def main():
             except IndexError:
                 print("Invalid taxi choice")
         elif menu_choice == "d":
-            pass
+            if current_taxi is not None:
+                current_taxi.start_fare()
+                drive_distance = int(input("Drive how far? "))
+                current_taxi.drive(drive_distance)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip will cost you ${trip_cost:.2f}")
+                bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
+
         else:
             print("Invalid option")
         print(MENU)
         menu_choice = input(">>> ").lower()
-
 
 
 def print_taxi_list(taxis):
