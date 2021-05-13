c5314a7c308c32fb4d122f88935fe2909edd74bc
AengusDavies <aengusdavies99@gmail.com>
2021-05-13 16:18:29 +1000

    Add new category variable

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -15,6 +15,12 @@ def main():
     for filename in os.listdir("."):
         if os.path.isdir(filename):
             continue
+        extension = filename.split(".")[-1]
+        if extension not in category_extension:
+            new_category = str(input(f"What category would you like to sort {extension} files into? "))
+            category_extension[extension] = new_category
+
+
 
 
 main()
