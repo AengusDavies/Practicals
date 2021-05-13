e592c48b451f1f59c197242eb235ade89ae582c9
AengusDavies <aengusdavies99@gmail.com>
2021-05-13 16:11:19 +1000

    Add exception handling for file path

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -3,7 +3,18 @@ CP1404/CP5632 Practical
 File Sorting - Second Iteration
 """
 
+import os
+
+
 def main():
+    category_extension = {}
+    try:
+        os.chdir("FilesToSort")
+    except FileExistsError:
+        pass
+    for filename in os.listdir("."):
+        if os.path.isdir(filename):
+            continue
 
 
 main()
