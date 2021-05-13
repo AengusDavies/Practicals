8fc31479c4d3092b9913dc0cd778e79025078590
AengusDavies <aengusdavies99@gmail.com>
2021-05-13 15:57:35 +1000

    Add file sorting to program

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -4,10 +4,25 @@ File Sorting - First Iteration
 """
 
 import os
+import shutil
 
 
 def main():
-    os.mkdir("FilesToSort")
+    try:
+        os.mkdir("FilesToSort")
+    except FileExistsError:
+        pass
+    for filename in os.listdir("."):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split(".")[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        file_addition = f"{filename}/{extension}"
+        os.rename(filename, file_addition)
+        shutil.move(filename, extension)
 
 
 main()
